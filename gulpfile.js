692f3127dc9cb0bf1c9f46a22e7ac56a9b3c3221
Tim Stratton <ec2-user@ip-172-31-5-177.us-east-2.compute.internal>
2019-06-28 03:50:17 +0000

    completed configuring unit tests for react and node. added instanbul/nyc for code coverage.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp');
 var jasmineBrowser = require('gulp-jasmine-browser');
+const jasmine = require('gulp-jasmine');
+var corejs = require('core-js');
 
-gulp.task('run_tests', function() {
-  return gulp.src(['src/**/*.js', 'spec/**/*.js'])
+
+gulp.task('run_route_tests', function() {
+  return gulp.src(['src/**/*.js','!src/WebApp/react/**/*.js','!src/WebApp/public/**/*.js','!gulpfile.js', 'tests/node/**/*.js'])
+    .pipe(jasmine());
+});
+
+gulp.task('run_react_tests', function() {
+  
+  return gulp.src(['tests/support/react.js' ,'tests/support/reactDOM.js','!gulpfile.js', 'tests/react/**/*.js'])
     .pipe(jasmineBrowser.specRunner({console: true}))
-    .pipe(jasmineBrowser.headless({driver: 'phantomjs'}));
+    .pipe(jasmineBrowser.headless({driver: 'chrome'}));
 });
